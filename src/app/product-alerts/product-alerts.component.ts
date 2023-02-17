dd653c01a56af41e5deba4a73cc75c549b12ebaa
Tom Deneau <tom.deneau@gmail.com>
2023-02-17 11:14:01 -0600

    pass event from product-alerts to product-list

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
-import {Product} from '../products';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
+
+
 export class ProductAlertsComponent {
-  @Input() product!: Product;
+  @Input() product: Product | undefined;
+  @Output() notify: EventEmitter<string>  = new EventEmitter<string>();
+
+  sendNotification(event: string) {
+     if (this.product) {
+        console.log(`in sendNotification for ${this.product.name} before emit`);
+     }                
+     this.notify.emit(event);
+     console.log("in sendNotification after emit");
+  }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,14 +5,20 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'], 
+  // template :   <product-alerts (notify)="onNotify()"> </product-alerts>
 })
 export class ProductListComponent {
-  products = products;
+    products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
-  }
+    share() {
+        window.alert('The product has been shared!');
+    }
+    onNotify() {
+        console.log("entering onNotify");
+        window.alert('You will be notified when the product goes on sale');
+        console.log("leaving onNotify");
+    }
 }
 
 
